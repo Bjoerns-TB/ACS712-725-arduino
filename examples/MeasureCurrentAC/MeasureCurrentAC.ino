d0d7d959ff290cfbb032f2a255fc61561d3a1fe8
Björn Amann <30447739+Bjoerns-TB@users.noreply.github.com>
2019-11-02 22:28:47 +0100

    Update MeasureCurrentAC.ino

--- examples/MeasureCurrentAC/MeasureCurrentAC.ino
+++ examples/MeasureCurrentAC/MeasureCurrentAC.ino
@@ -14,7 +14,7 @@ long readVcc() {
   long result;
   // Read 1.1V reference against AVcc
   ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  delay(2); // Wait for Vref to settle
+  delay(10); // Wait for Vref to settle
   ADCSRA |= _BV(ADSC); // Convert
   while (bit_is_set(ADCSRA,ADSC));
   result = ADCL;
