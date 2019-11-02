f8aeda06f0ec0f201dc692adde7e52f8825ad03a
Björn Amann <30447739+Bjoerns-TB@users.noreply.github.com>
2019-11-02 17:26:37 +0100

    Add readVcc()

--- examples/MeasureCurrentAC/MeasureCurrentAC.ino
+++ examples/MeasureCurrentAC/MeasureCurrentAC.ino
@@ -8,6 +8,19 @@
 // Replace with your version if necessary
 ACS712 sensor(ACS712_30A, A1);
 
+long readVcc() {
+  long result;
+  // Read 1.1V reference against AVcc
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA,ADSC));
+  result = ADCL;
+  result |= ADCH<<8;
+  result = 1125300L / result; // Back-calculate AVcc in mV
+  return result;
+}
+
 void setup() {
   Serial.begin(9600);
 
